e47c9823527d55b15ccabd93ffc0089932ec96bf
Saeed Shaban <aposara.acp@gmail.com>
2019-10-27 09:34:02 +0300

    first upload

---
+++ app.js
@@ -0,0 +1,186 @@
+function Abandon(){
+    document.getElementById('Abandon').innerHTML="هجر";
+}
+
+
+
+
+function Annual(){
+    document.getElementById('Annual').innerHTML = "سنوي";
+}
+
+
+function Academy(){
+    document.getElementById('Academy').innerHTML = "معهد";
+}
+
+
+function Access(){
+    document.getElementById('Access').innerHTML = "الوصول إلي";
+}
+
+
+function Accommodate(){
+    document.getElementById('Accommodate').innerHTML = "تتسع ";
+}
+
+
+function Accompany(){
+    document.getElementById('Accompany').innerHTML = "رافق";
+}
+
+
+function Accumulate(){
+    document.getElementById('Accumulate').innerHTML = "تراكم";
+}
+
+
+function Accurate(){
+    document.getElementById('Accurate').innerHTML = "دقيق - حاسم";
+}
+
+
+function Achive(){
+    document.getElementById('Achive').innerHTML = "تحقيق";
+}
+
+function Acknowledge(){
+    document.getElementById('Acknowledge').innerHTML = "اعتراف";
+}
+
+
+function Acquire(){
+    document.getElementById('Acquire').innerHTML = "حاز";
+}
+
+function Adapt(){
+    document.getElementById('Adapt').innerHTML = "تكيف";
+}
+
+function Adequate(){
+    document.getElementById('Adequate').innerHTML = "كاف - يكفي";
+}
+
+function Administrate(){
+    document.getElementById('Administrate').innerHTML = "إدارة";
+}
+
+
+function Adjacent(){
+    document.getElementById('Adjacent').innerHTML = "مجاور";
+}
+
+function Adjust(){
+    document.getElementById('Adjust').innerHTML = "ضبط";
+}
+
+function Adult(){
+    document.getElementById('Adult').innerHTML = "بالغ";
+}
+
+
+function Advocate(){
+    document.getElementById('Advocate').innerHTML = "دفاع";
+}
+
+function Advocate(){
+    document.getElementById('Advocate').innerHTML = "دفاع";
+}
+
+function Affect(){
+    document.getElementById('Affect').innerHTML = "تؤثر";
+}
+
+function Aggregate(){
+    document.getElementById('Aggregate').innerHTML = "مجموع";
+}
+
+function Analysis(){
+    document.getElementById('Analysis').innerHTML = "تحليل";
+}
+
+function Albeit(){
+    document.getElementById('Albeit').innerHTML = "وإن يكن";
+}
+
+function Allocate(){
+    document.getElementById('Allocate').innerHTML = "تخصيص";
+}
+
+function Alternative(){
+    document.getElementById('Alternative').innerHTML = "بديل";
+}
+
+function Ambiguous(){
+    document.getElementById('Ambiguous').innerHTML = "غموض";
+}
+
+
+function Amend(){
+    document.getElementById('Amend').innerHTML = "تعديل";
+}
+
+
+function Append(){
+    document.getElementById('Append').innerHTML = "أضاف";
+}
+
+
+function Appreciate(){
+    document.getElementById('Appreciate').innerHTML = "يقدر";
+}
+
+
+
+function Approach(){
+    document.getElementById('Approach').innerHTML = "مقاربة";
+}
+
+function Appromixate(){
+    document.getElementById('Appromixate').innerHTML = "تقريبي";
+}
+
+
+function Arbitrary(){
+    document.getElementById('Arbitrary').innerHTML = "عشوائي";
+}
+
+
+
+function Area(){
+    document.getElementById('Area').innerHTML = "منطقة";
+}
+
+
+
+function Aspect(){
+    document.getElementById('Aspect').innerHTML = "مظهر";
+}
+
+
+function Assemble(){
+    document.getElementById('Assemble').innerHTML = "جمع";
+}
+
+
+function Assess(){
+    document.getElementById('Assess').innerHTML = "اقترب";
+}
+
+
+function Assign(){
+    document.getElementById('Assign').innerHTML = "تعيين";
+}
+
+
+
+
+function Assisst(){
+    document.getElementById('Assisst').innerHTML = "مساعدة";
+}
+
+
+
+function Assume(){
+    document.getElementById('Assume').innerHTML = "افترض";
+}
